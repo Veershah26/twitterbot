6eb79c79de52cc45808df3c89b4dccc372041012
Veershah26 <shah.veer18@gmail.com>
2021-06-06 18:06:32 +0530

    added basic auth, & done basic api check

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,14 @@
+import tweepy
+import tkinter
 
+consumer_key = 'your_consumer_key_here'
+consumer_secret = 'your_consumer_secret_key_here'
+access_token = 'your_access_token_here'
+access_token_secret = 'your_secret_access_token_here'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print (user.name)
